fe7b0597156a0340acd32309c4668128d8ac6305
abhinav3254 <abhinav3254@gmail.com>
2022-11-26 11:25:44 +0530

    let's start

--- NATURE API/working/app.js
+++ NATURE API/working/app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = 4500;
 
-app.get('/', (req, res) => {
-    // res.status(200).send('Hello from the server side!!');
-    res
-        .status(200)
-        .json({ name: 'Abhinav Jha', message: 'Hello from server side' });
-});
+// app.get('/', (req, res) => {
+//     // res.status(200).send('Hello from the server side!!');
+//     res
+//         .status(200)
+//         .json({ name: 'Abhinav Jha', message: 'Hello from server side' });
+// });
+
+// let start
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}....`);
